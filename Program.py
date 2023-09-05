4d5822a67670cc174421fb053255fa3fcfa89de8
ezrabehr <ezra.behr@gmail.com>
2023-09-05 18:11:02 +0300

    factorial

--- Program.py
+++ Program.py
@@ -1,7 +1,20 @@
 # adding numbers from 1 till 100
 
-if __name__=="__main__":
+
+def factorial(number: int) -> int:
+    factorial_number = 1
+    for x in range(1, number+1):
+        factorial_number *= x
+    return factorial_number
+
+
+if __name__ == "__main__":
     added_numbers = 0
     for index in range(101):
         added_numbers += index
     print(added_numbers)
+
+    print(f'5 {factorial(5)}')
+    print(f'6 {factorial(6)}')
+    print(f'7 {factorial(7)}')
+    print(f'8 {factorial(8)}')

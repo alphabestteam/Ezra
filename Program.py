bb30217d8e736242f9f44a9a298657dfbc91c0cc
ezrabehr <ezra.behr@gmail.com>
2023-09-06 09:33:45 +0300

    layout fixing

--- Program.py
+++ Program.py
@@ -1,9 +1,6 @@
-# adding numbers from 1 till 100
-
-
 def factorial(number: int) -> int:
     factorial_number = 1
-    for x in range(1, number+1):
+    for x in range(1, number + 1):
         factorial_number *= x
     return factorial_number
 
@@ -14,7 +11,7 @@ if __name__ == "__main__":
         added_numbers += index
     print(added_numbers)
 
-    print(f'5 {factorial(5)}')
-    print(f'6 {factorial(6)}')
-    print(f'7 {factorial(7)}')
-    print(f'8 {factorial(8)}')
+    print(f"5 {factorial(5)}")
+    print(f"6 {factorial(6)}")
+    print(f"7 {factorial(7)}")
+    print(f"8 {factorial(8)}")

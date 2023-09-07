d3bc337ffd4494b80c455a1af9bcd4b8be7617d8
ezrabehr <ezra.behr@gmail.com>
2023-09-07 16:41:25 +0300

    moved the main code to 13

---
+++ 13.py
@@ -0,0 +1,25 @@
+from Person import Person
+
+
+if __name__ == "__main__":
+    person1 = Person()
+    person2 = Person()
+    person3 = Person()
+    person4 = Person()
+    person5 = Person()
+    person6 = Person()
+    person7 = Person()
+    person8 = Person()
+    person9 = Person()
+    person10 = Person()
+
+    print(person1)
+    print(person2)
+    print(person3)
+    print(person4)
+    print(person5)
+    print(person6)
+    print(person7)
+    print(person8)
+    print(person9)
+    print(person10)
--- Person.py
+++ Person.py
@@ -194,25 +194,4 @@ class Person:
         self._age = new_age
 
 
-if __name__ == "__main__":
-    person1 = Person()
-    person2 = Person()
-    person3 = Person()
-    person4 = Person()
-    person5 = Person()
-    person6 = Person()
-    person7 = Person()
-    person8 = Person()
-    person9 = Person()
-    person10 = Person()
 
-    print(person1)
-    print(person2)
-    print(person3)
-    print(person4)
-    print(person5)
-    print(person6)
-    print(person7)
-    print(person8)
-    print(person9)
-    print(person10)

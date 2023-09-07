d6a299ef68061ea15ddecf50d2e2a8f1a147ec2e
ezrabehr <ezra.behr@gmail.com>
2023-09-07 16:36:50 +0300

    finished 3 of list comprehension

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -1,13 +1,12 @@
 if __name__ == "__main__":
-
     good_numbers = False
 while not good_numbers:
     try:
-        num1 = int(input('enter first number: '))
-        num2 = int(input('enter second number: '))
+        num1 = int(input("enter first number: "))
+        num2 = int(input("enter second number: "))
         good_numbers = True
     except ValueError:
-        print('please enter valid numbers')
+        print("please enter valid numbers")
 
 print(num1, num2)
 print("Finished running")
---
+++ List Comprehension/3.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    top_num = int(input("enter the number: "))
+    sqrt_list = [num**2 for num in range(1, top_num + 1)]
+    print(sqrt_list)

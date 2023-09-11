a6a6a52148b3e8cb15623a955e98316598caa984
ezrabehr <ezra.behr@gmail.com>
2023-09-11 12:09:45 +0300

    finished 4 in file handling

---
+++ File Handling/4/4.py
@@ -0,0 +1,19 @@
+import json
+
+
+config_json = open("Ezra/File Handling/4/config.json", "r")
+
+p_config_json = json.load(config_json)
+
+data_upper = p_config_json["data"].upper()
+
+if p_config_json["silent"] == True:
+    print(data_upper)
+
+p_config_json["data"] = data_upper
+
+# writing back in
+config_json = open("Ezra/File Handling/4/config.json", "w")
+config_json.write(json.dumps(p_config_json))
+
+

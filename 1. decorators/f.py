ead1930258ff8e84a8325cf86a86aee528ae1d75
ezrabehr <ezra.behr@gmail.com>
2023-09-13 17:41:33 +0300

    finished f

--- 1. decorators/f.py
+++ 1. decorators/f.py
@@ -1,7 +1,26 @@
-def timer(func) -> function:
-    pass
+import time
+
+
+def timer(func):
+    def inner():
+        start_time = time.time()  # get's the time
+        func()
+        end_time = time.time()
+        print(end_time - start_time)
+
+    return inner
 
 
 @timer
-def random_function() -> None:
-    pass
+def random_function():
+    print("doing something")
+    for x in range(100):
+        print("*" * x)
+
+
+def main():
+    random_function()
+
+
+if __name__ == "__main__":
+    main()

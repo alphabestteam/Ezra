81f3f6c6cbbde8897111d5dbda83e2ad331b6183
ezrabehr <ezra.behr@gmail.com>
2023-09-21 16:00:23 +0300

    finished PDF 3

--- PDF 2/main.py
+++ PDF 2/main.py
@@ -1,9 +1,12 @@
 import pymongo
 
+
 def print_database(mydb):
     for collections in mydb.list_collection_names():
+        print(collections)
         for document in mydb[collections].find():
             print(document)
+
 
 def main():
     connection_string = "mongodb://localhost:27017/"
@@ -30,16 +33,16 @@ def main():
     ]
 
     friends_group = [
-        { "_id": 1, "name": "Amit", "age": 20, "role": "waiter"},
-        { "_id": 2, "name": "Yonatan", "age": 24, "role": "doctor"},
-        { "_id": 3, "name": "Noa", "age": 22, "role": "student"},
-        { "_id": 4, "name": "Michal", "age": 21, "role": "baker"},
+        {"_id": 1, "name": "Amit", "age": 20, "role": "waiter"},
+        {"_id": 2, "name": "Yonatan", "age": 24, "role": "doctor"},
+        {"_id": 3, "name": "Noa", "age": 22, "role": "student"},
+        {"_id": 4, "name": "Michal", "age": 21, "role": "baker"},
     ]
 
     x = army.delete_many({})
     y = family.delete_many({})
     z = friends.delete_many({})
-    
+
     # first way
     for person in army_group:
         army_insert = army.insert_one(person)
@@ -52,11 +55,43 @@ def main():
 
     print_database(mydb)
 
-    print('deleting Lihi')
-    my_query = {"name" : "Lihi"}
+    # deleting Lihi
+    print("deleting Lihi")
+    my_query = {"name": "Lihi"}
     army.delete_one(my_query)
+    print_database(mydb)
+
+    # finding good soldier
+    print("finding good soldier")
+    soldier_query = {"role": "DevOps", "age": {"$lt": 23}}
+    gabi = army.find_one(soldier_query)
+    print(gabi["name"])
+
+    gabi_age = gabi["age"]
+
+    new_role = army.find_one({"age": gabi_age, "_id": {"$ne": gabi["_id"]}})
+    print(new_role)
+    army.update_one({"_id" : gabi["_id"]}, {"$set" : {"role" : new_role["role"]}})
+    print_database(mydb)
+
+    print('-------------')
+
+    # moving 
+    sorted_army_friends = army.aggregate([{"$sort" : {"age" : -1}}, {"$out" : "army"}])
+
+
+    for x in sorted_army_friends:
+        print(x)
+
+    delete_query = {"age" : {"$gt" : 23}}
+    moving_soldiers = army.find(delete_query)
+    add_to_friend = friends.insert_many(moving_soldiers)
+    print_database(mydb)
+    moved_from_army = army.delete_many(delete_query)
 
     print_database(mydb)
+
+
 
 if __name__ == "__main__":
     main()

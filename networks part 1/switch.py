4740832ceef4f1f20dbafe00679e2aeb6e5f983b
ezrabehr <ezra.behr@gmail.com>
2023-10-10 17:29:59 +0300

    fixed bug

--- networks part 1/switch.py
+++ networks part 1/switch.py
@@ -10,16 +10,23 @@ port_mac = {
     "9c:7b:ef:aa:2b:b7": "Eth5",
     "ec:b1:d7:5b:a1:b4": "Eth6",
     "70:5a:0d:4a:cd:c7": "Eth7",
-    "ff:ff:ff:ff:ff:ff": "Eth0-7"
 }
 
-with open('networks part 1\output.csv', 'w', newline='') as output_file:
+with open("networks part 1\output.csv", "w", newline="") as output_file:
     csv_writer = csv.writer(output_file)
-    csv_writer.writerow(['interface', 'source_mac', 'dest_mac'])
+    csv_writer.writerow(["interface", "source_mac", "dest_mac"])
 
     with open("networks part 1\input.csv", mode="r") as input_file:
         csvFile = csv.DictReader(input_file)
 
         for lines in csvFile:
-            new_interface = port_mac[lines['dest_mac']]
-            csv_writer.writerow([new_interface, lines['source_mac'], lines['dest_mac']])
+            if lines["dest_mac"] == "ff:ff:ff:ff:ff:ff":
+                exclude_port = lines["source_mac"]
+                for key, value in port_mac.items():
+                    if key != exclude_port:
+                        csv_writer.writerow([value, lines["source_mac"], key])
+            else:
+                new_interface = port_mac[lines["dest_mac"]]
+                csv_writer.writerow(
+                    [new_interface, lines["source_mac"], lines["dest_mac"]]
+                )

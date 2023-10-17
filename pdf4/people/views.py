fcaffa5908052b6f6789d85617471a40d34e684d
ezrabehr <ezra.behr@gmail.com>
2023-10-17 16:39:39 +0300

    finished pdf4

--- pdf4/people/views.py
+++ pdf4/people/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from .serializers import PersonSerializer
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -13,6 +14,7 @@ def get_all_people(request):
     people_list = list(Person.objects.all().values())
     return JsonResponse(people_list, status=status.HTTP_200_OK, safe=False)
 
+
 @csrf_exempt
 def add_person(request):
     if request.method == "POST":
@@ -20,27 +22,35 @@ def add_person(request):
         data_deserialized = PersonSerializer(data=data)
         if data_deserialized.is_valid():
             data_deserialized.save()
-            return HttpResponse('saved!', status=status.HTTP_201_CREATED)
+            return HttpResponse("saved!", status=status.HTTP_201_CREATED)
         else:
-            return HttpResponse('not good', status=status.HTTP_400_BAD_REQUEST)
+            return HttpResponse("not good", status=status.HTTP_400_BAD_REQUEST)
     else:
-        return HttpResponse('not POST', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return HttpResponse("not POST", status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-@csrf_exempt    
+
+@csrf_exempt
 def remove_person(request, tz):
-    if request.method == 'DELETE':
+    if request.method == "DELETE":
         person = Person.objects.get(tz=tz)
         person.delete()
-        return HttpResponse('deleted!', status=status.HTTP_200_OK)
+        return HttpResponse("deleted!", status=status.HTTP_200_OK)
+
 
 @csrf_exempt
 def update_person(request):
-    if request.method == 'PUT':
-        update_p = JSONParser().parse(request)
-        update_p_serialized = PersonSerializer(data=update_p)
-        if update_p_serialized.is_valid():
-            updated = Person.objects.get(tz= update_p_serialized["tz"])
-            update_p_serialized.update(updated, update_p)
-            return HttpResponse('updated!', status=status.HTTP_200_OK)
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        try:
+            person = Person.objects.get(tz=data["tz"])
+        except ObjectDoesNotExist:
+            return HttpResponse(
+                "object doesn't exist", status=status.HTTP_404_NOT_FOUND
+            )
+        person_ser = PersonSerializer(person, data=data)
+
+        if person_ser.is_valid():
+            person_ser.save()
+            return HttpResponse("updated!", status=status.HTTP_200_OK)
         else:
-            return HttpResponse('not updated', status=status.HTTP_400_BAD_REQUEST)
+            return HttpResponse("not updated", status=status.HTTP_400_BAD_REQUEST)

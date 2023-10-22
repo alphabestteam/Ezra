6ba1ae22da0338019a7d419960709568c641311d
ezrabehr <ezra.behr@gmail.com>
2023-10-22 13:43:29 +0300

    test_get_all_parents

--- pdf4/people/tests.py
+++ pdf4/people/tests.py
@@ -1,16 +1,33 @@
-from django.test import TestCase
-from .models import Parent, Person
+import json
+from django.test import TestCase, Client
+from .models import Person
 
 
 # Create your tests here.
 class PersonTestCase(TestCase):
+    fixtures = ['people_fixture.json']
+
     def setUp(self):
-        Person.objects.create(name="Shira", tz= 4,date_of_birth= "2001-08-09",city= "Rishon LeZion")
-        Person.objects.create(name="Yoav", tz=19, date_of_birth = "1989-08-21", city="Herzliya")
+        Person.objects.create(name="qwe", tz= 94354,date_of_birth= "2001-08-09",city= "Rishon LeZion")
+        Person.objects.create(name="Yoavi", tz=139, date_of_birth = "1989-08-21", city="Herzliya")
+        self.client = Client()
 
     def test_is_over_18(self):
         """Checks if person is over 18 years old"""
-        shira = Person.objects.get(name="Shira")
-        yoav = Person.objects.get(name="Yoav")
-        self.assertEqual(shira.test_person_is_over_18(), True)
-        self.assertEqual(yoav.test_person_is_over_18(), True)
+        qwe = Person.objects.get(name="qwe")
+        yoavi = Person.objects.get(name="Yoavi")
+        self.assertTrue(qwe.test_person_is_over_18())
+        self.assertTrue(yoavi.test_person_is_over_18())
+
+    def test_get_all_parents(self):
+        """Checks to see if getAllParents return 200 and the right length"""
+        response = self.client.get("/api/getAllParents/")
+        self.assertEqual(response.status_code, 200)
+
+        print("*****************")
+        print(response.content)
+        json_data = json.loads(response.content)
+        self.assertEqual(len(json_data), 7)
+
+    
+        

5847dccb39fa397a0c83ba71bae955e206043fa9
ezrabehr <ezra.behr@gmail.com>
2023-11-06 16:20:25 +0200

    added attribute and finished

--- pdf 15/files to use/script.js
+++ pdf 15/files to use/script.js
@@ -1,16 +1,21 @@
 const button = document.getElementById('the-button');
+// const main = document.querySelector("main");
 const bobGif = document.getElementById("bob");
+
 
 
 const toggleBob = function(){
     //implement this function
-    if (button.textContent == "Hide Bob ;)"){
+    if (bobGif.getAttribute('show-bob') == "true"){
         bobGif.style.display = 'none';
         button.textContent = "Show me Bob ;)";
+        bobGif.setAttribute('show-bob', 'false');
     }
     else {
         bobGif.style.display = 'block';
         button.textContent = "Hide Bob ;)";
+        bobGif.setAttribute('show-bob', 'true');
+
     }
     
 };

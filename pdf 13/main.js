11b838312a11a2c3c7925e448dcb23685cc5ea7d
ezrabehr <ezra.behr@gmail.com>
2023-11-06 11:58:57 +0200

    finished question 18

--- pdf 13/main.js
+++ pdf 13/main.js
@@ -1,24 +1,42 @@
-const main_heading = document.getElementById('main-heading');
+/*3*/ const main_heading = document.getElementById('main-heading');
 
-console.log(main_heading.id);
-console.log(main_heading.className);
-console.log(main_heading.classList);
-console.log(main_heading.dataset);
-console.log(main_heading.getAttribute('nonStandard'));
-console.log(main_heading.classList.add('border','bg-lightcyan'));
+/*4*/ console.log(main_heading.id);
+/*5*/ console.log(main_heading.className);
+/*6*/ console.log(main_heading.classList);
+/*7*/ console.log(main_heading.dataset);
+/*8*/ console.log(main_heading.getAttribute('nonStandard'));
+      console.log(main_heading.classList.add('border','bg-lightcyan'));
+
+/*9*/ let content = main_heading.textContent;
+      console.log(content);
+/*10*/ console.log(content.trim());
+
+/*11*/ content = main_heading.textContent = "Hello there pearl!";
+
+/*12*/ const lineBreak = document.createElement('br');
+       const spanElement = document.createElement('span');
+       const addSentence = document.createTextNode(`it's me SpongeBob!`);
+
+       main_heading.appendChild(lineBreak);
+       // adding the text to a span element first.
+       spanElement.appendChild(addSentence);
+       main_heading.appendChild(spanElement);
 
 
-console.log(main_heading.textContent);
-console.log(main_heading.textContent.trim());
+/*13*/ console.log(main_heading);
 
-main_heading.textContent = "Hello there pearl!";
-const span = document.createElement("span");
+/*14*/ const cloned = main_heading.cloneNode(true);
+       console.log(cloned);
 
-main_heading.textContent += "<br><span>its me SpongeBob!</span>";
+/*15*/ const subHeading = document.createElement('h2');
+       subHeading.textContent = "jellyfish hunting is the best";
 
+/*16*/ document.body.appendChild(subHeading);
 
+/*17*/ const lorem_ipsum ="Lorem ipsum dolor sit amet consectetur adipisicing elit." +
+                "Labore eum, earum deserunt numquam quis explicabo. Delectus" +
+                "id, cum voluptate dicta aperiam sunt voluptatum quis eaque" +
+                "aliquam distinctio reiciendis iste minima?";
 
-
-
-
-
+/*18*/ lorem_arr = lorem_ipsum.split(" ");
+console.log(lorem_arr);

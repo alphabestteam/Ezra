fdd625af895fc21705a7adca595c58c976a95607
ezrabehr <ezra.behr@gmail.com>
2023-11-06 16:01:28 +0200

    finished pdf 15

--- pdf 15/files to use/script.js
+++ pdf 15/files to use/script.js
@@ -1,7 +1,18 @@
 const button = document.getElementById('the-button');
-const main = document.querySelector("main");
 const bobGif = document.getElementById("bob");
+
 
 const toggleBob = function(){
     //implement this function
+    if (button.textContent == "Hide Bob ;)"){
+        bobGif.style.display = 'none';
+        button.textContent = "Show me Bob ;)";
+    }
+    else {
+        bobGif.style.display = 'block';
+        button.textContent = "Hide Bob ;)";
+    }
+    
 };
+
+button.addEventListener('click',toggleBob);

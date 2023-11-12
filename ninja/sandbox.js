7a7ded465892e02821fc33bbbd357ad0f5757f03
ezrabehr <ezra.behr@gmail.com>
2023-11-12 19:33:28 +0200

    started learning from ninja

---
+++ ninja/sandbox.js
@@ -0,0 +1,4 @@
+var names = ['david', 'moshe', 'avi'];
+names.push('max');
+names.push(32);
+console.log(names);
---
+++ ninja/sandbox.ts
@@ -0,0 +1,6 @@
+let names : (number | string)[] = ['david', 'moshe', 'avi'];
+names.push('max');
+names.push(32); 
+
+console.log(names);
+ 

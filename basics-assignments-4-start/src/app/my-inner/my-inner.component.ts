c769bc499ecddab9764496be0f96a9d9d81c2c50
ezrabehr <ezra.behr@gmail.com>
2023-11-20 23:25:46 +0200

    finished question 4

--- basics-assignments-4-start/src/app/my-inner/my-inner.component.ts
+++ basics-assignments-4-start/src/app/my-inner/my-inner.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-my-inner',
   templateUrl: './my-inner.component.html',
-  styleUrls: ['./my-inner.component.css']
+  styleUrls: ['./my-inner.component.css'],
 })
 export class MyInnerComponent {
-
+  @Input() startingNumber: number = 0;
+  @Output() addOrSub10: EventEmitter<number> = new EventEmitter();
+  addClicked() {
+    this.startingNumber += 1;
+    this.check10();
+  }
+  subClicked() {
+    this.startingNumber -= 1;
+    this.check10();
+  }
+  check10() {
+    if (this.startingNumber === 10 || this.startingNumber === -10) {
+      this.addOrSub10.emit(this.startingNumber);
+      this.startingNumber = 0;
+    }
+  }
 }
--- basics-assignments-4-start/src/app/my-outer/my-outer.component.ts
+++ basics-assignments-4-start/src/app/my-outer/my-outer.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-outer',
   templateUrl: './my-outer.component.html',
-  styleUrls: ['./my-outer.component.css']
+  styleUrls: ['./my-outer.component.css'],
 })
 export class MyOuterComponent {
-
+  outerStartingNumber: number = 5;
+  total: number = 0;
+  addOrSub10(num: number) {
+    this.total += num;
+  }
 }
